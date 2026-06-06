8cd71ef75a206e224670cca142de74a8cf09f4d7
Dacianobrian <dacianobrian.17@gmail.com>
2026-06-06 13:24:19 -0300

    Fix Supabase public key and add data diagnostics

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BRIAN_FINANCE_CONFIG = {
   supabaseUrl: "https://sdyljxblbpqharclfyme.supabase.co",
-  supabaseAnonKey: "sb_publishable_YLZ3Bk8SmUwpHjOli5FGBg_W2FX4ZyH",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkeWxqeGJsYnBxaGFyY2xmeW1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0NTc2MzQsImV4cCI6MjA5NjAzMzYzNH0.0iwKSHBA1DcPzqJJB9G9wSCq3TD9LOm4K-8X0kIw9n0",
 };
